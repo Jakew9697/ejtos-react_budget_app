427a4585b93bb0ee9c6d2ac4bbe9d7b10d2a4885
Jake Worsham <jworsham9697@gmail.com>
2024-01-11 19:37:07 -0500

    created budget allocation app, changed the delete feature into a button because it looks better and makes more sense. Also, fixed an error found in the instructions where the remaining budget was not being updated by the input of the budget.

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,24 @@
+import React, {useContext, useState} from "react";
+import {AppContext} from "../context/AppContext";
 
+const Budget = () => {
+    const {budget, dispatch} = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+
+    const handleBudgetChange = (event) => {
+        const updatedBudget = parseInt(event.target.value, 10);
+        setNewBudget(updatedBudget);
+
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: updatedBudget,
+        });
+    }
+    return (
+<div className='alert alert-secondary'>
+<span>Budget: £</span>
+<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+</div>
+    );
+};
+export default Budget;
